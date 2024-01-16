be4e4c2240e8306b8c6f130bc4f34c9378417bcb
alejandro delarosa <a.delarosa9@yahoo.com>
2024-01-15 19:40:36 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json))
+    .catch((error) => console.error('Error fetching books:', error));
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML = book.title; // Assuming the title property is present in the book object
     main.appendChild(h2);
   });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
